746c3870c714419a4adf72feca489dc7d2bf111f
Wallace Soares <wallace.soares@incognia.com>
2026-05-14 09:39:52 -0300

    Adding new payment types (#92)

--- request_types.go
+++ request_types.go
@@ -145,6 +145,9 @@ const (
 	NuPay          paymentMethodType = "nu_pay"
 	Paypal         paymentMethodType = "paypal"
 	Pix            paymentMethodType = "pix"
+	PixRefund      paymentMethodType = "pix_refund"
+	TEF            paymentMethodType = "tef"
+	TED            paymentMethodType = "ted"
 )
 
 type CardInfo struct {
